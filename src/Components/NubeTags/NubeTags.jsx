60f31956838263a4431d232fe0026a3ba4e4719d
RobertoGuzman <robertoguzman64@gmail.com>
2022-07-03 16:00:59 +0200

    feat: Añadido nuevo componente NubeTags al proyecto

---
+++ src/Components/NubeTags/NubeTags.jsx
@@ -0,0 +1,57 @@
+import { TagCloud } from 'react-tagcloud'
+import { minMaxRoundedRandom } from '../../utiles';
+import './NubeTags.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { baseURL } from '../../utiles';
+
+const NubeTags = () => {
+    
+    const [tweets, setTweets] = useState([]);
+    const [msgError, setMsgError] = useState('');
+    let data = [];
+
+    useEffect(() => {
+        traerTweets();
+    }, []);
+
+    useEffect(() => {
+    }, [tweets]);
+
+    const traerTweets = async () => {
+        try {
+            let resultado = await axios.get(`${baseURL}twitter`);
+            setTweets(resultado.data);
+            console.log(resultado.data)
+        } catch (error) {
+            setMsgError(error);
+        }
+    };
+
+    data = tweets.map((tweet) => {
+        return {
+            key: Math.random(),
+            value: tweet.user.name,
+            count: minMaxRoundedRandom(18, 58)
+        };
+    });
+
+    const customRenderer = (tag) => {
+        tag.map((tag) => {
+            return (
+                <div key={tag.value}>
+                    {tag.value}
+                </div>
+            );
+        });
+    };
+
+    return (
+        <div className='componenteNubeTags'>
+            <TagCloud tags={data} minSize={5} maxSize={10} renderer={customRenderer(data)} />
+            {msgError}
+        </div>
+    );
+}
+
+export default NubeTags;
