5b673cbfba6eb893a619de26873806cd86ff118d
RobertoGuzman <robertoguzman64@gmail.com>
2022-07-03 16:26:35 +0200

    feat: Añadida nueva función al archivo utiles.jsx

--- src/utiles.jsx
+++ src/utiles.jsx
@@ -1,1 +1,5 @@
 export const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/';
+
+export const minMaxRoundedRandom = (min, max) => {
+    return Math.round(Math.random() * (max - min) + min);
+}
